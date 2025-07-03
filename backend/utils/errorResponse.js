dcf6f51293fae6f15c101101c3432c18287c862e
xlexandr0 <alexandro.alarcon@tecsup.edu.pe>
2025-07-03 17:47:27 -0500

    Add missing errorResponse module

---
+++ backend/utils/errorResponse.js
@@ -0,0 +1,9 @@
+// backend/utils/errorResponse.js
+class ErrorResponse extends Error {
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
+  }
+}
+
+module.exports = ErrorResponse;
